254d24de84217a9744d519bde4aa06207d3c5547
Brittany Yee <brittanyyee1@gmail.com>
2019-12-08 23:05:06 -0500

    cleaned up comments

--- database/parsePhotos.js
+++ database/parsePhotos.js
@@ -18,53 +18,6 @@ rl.on("close", () => {
   console.log("done");
 });
 
-// for (let i = 0; i < storage.length; i++) {
-//   let row = storage[i];
-
-//   //if row has correct amt of cols &&
-//   if (row.length === 4) {
-//     writeStream.write(row);
-//   } else {
-//     //else it write to bad row & handle rows later
-//     writeStream2.write(row)
-//   }
-// }
-
-// const csvStream = csv2.parse();
-
-// let currentRow = "";
-
-// csvStream
-//   .on("data", function (data) {
-//     console.log("regular data", data);
-//     currentRow = data;
-
-//     let firstLetter = data[2].charAt(0);
-//     console.log("firstLetter", firstLetter);
-//     let lastLetter = data[2].charAt(data[2].length - 1)
-//     console.log("lastLetter", lastLetter);
-
-//     //if the beginning or ending " is missing,
-//     //write to badphotos csv
-//     if (firstLetter !== '\"' || lastLetter !== '\"') {
-//       let modifiedData = JSON.stringify(data);
-//       writeStream2.write(modifiedData + "\n");
-//     } else {
-//       //else, write to newPhotos
-//       let modifiedData = JSON.stringify(data);
-//       writeStream.write(modifiedData + "\n");
-//     }
-
-//   })
-//   .on("end", function () {
-//     console.log("done");
-//   })
-//   .on("error", function (error) {
-//     console.log("current here", currentRow);
-//   });
-
-// readStream.pipe(csvStream);
-
 function arrayFromCSV(data) {
   var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
   var a = [];
